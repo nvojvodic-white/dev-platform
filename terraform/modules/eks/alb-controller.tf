97de522002b9991413d3eabb25c3f4fd2b1bdb8d
Nikola Vojvodic <n.vojvodic@icloud.com>
2026-04-12 15:37:15 +0200

    feat: add AWS Load Balancer Controller and public ALB ingress

---
+++ terraform/modules/eks/alb-controller.tf
@@ -0,0 +1,39 @@
+data "http" "alb_controller_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_policy" "alb_controller" {
+  name        = "${var.project_name}-${var.environment}-alb-controller"
+  description = "Policy for AWS Load Balancer Controller"
+  policy      = data.http.alb_controller_policy.response_body
+}
+
+resource "aws_iam_role" "alb_controller" {
+  name = "${var.project_name}-${var.environment}-alb-controller"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
+
+output "alb_controller_role_arn" {
+  value = aws_iam_role.alb_controller.arn
+}
