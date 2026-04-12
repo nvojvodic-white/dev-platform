7b9e125da59a8a7a752dbc48202034e5f37bc291
Nikola Vojvodic <n.vojvodic@icloud.com>
2026-04-12 14:21:48 +0200

    fix: add EKS access entry and policy to Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,19 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
 }
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = "arn:aws:iam::375976227140:user/terraform-admin"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = "arn:aws:iam::375976227140:user/terraform-admin"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
