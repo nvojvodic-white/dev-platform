e756b45f14b7858a1083e9e6d13e4230db4cbc42
Nikola Vojvodic <n.vojvodic@icloud.com>
2026-04-12 15:28:45 +0200

    feat: add IRSA for sample-app with least-privilege S3 access verified

---
+++ terraform/modules/eks/irsa-sample-app.tf
@@ -0,0 +1,49 @@
+data "aws_iam_policy_document" "sample_app" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::dev-platform-tfstate-375976227140",
+      "arn:aws:s3:::dev-platform-tfstate-375976227140/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "sample_app" {
+  name        = "${var.project_name}-${var.environment}-sample-app"
+  description = "Least-privilege policy for sample-app pods"
+  policy      = data.aws_iam_policy_document.sample_app.json
+}
+
+resource "aws_iam_role" "sample_app" {
+  name = "${var.project_name}-${var.environment}-sample-app"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:default:sample-app"
+          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sample_app" {
+  role       = aws_iam_role.sample_app.name
+  policy_arn = aws_iam_policy.sample_app.arn
+}
+
+output "sample_app_role_arn" {
+  value = aws_iam_role.sample_app.arn
+}
